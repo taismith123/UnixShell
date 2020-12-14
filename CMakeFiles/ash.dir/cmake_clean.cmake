31277eb2af8bcc149e11c44eeb2b852a658e16cc
taismith123 <42554501+taismith123@users.noreply.github.com>
2020-12-14 09:46:24 -0500

    Add files via upload

---
+++ CMakeFiles/ash.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ash.dir/ash.cpp.o"
+  "CMakeFiles/ash.dir/jcode.cpp.o"
+  "CMakeFiles/ash.dir/test.cpp.o"
+  "CMakeFiles/ash.dir/testing.cpp.o"
+  "ash.pdb"
+  "ash"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
